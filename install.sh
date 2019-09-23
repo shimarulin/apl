c8600cbfef4018bb16710181a43ff4a2fe29c722
Vyacheslav Shimarulin <shimarulin@gmail.com>
2019-09-24 02:23:02 +0300

    feat: install ansible via pip

--- install.sh
+++ install.sh
@@ -31,9 +31,8 @@ install_deps () {
     sudo apt install --yes \
       software-properties-common \
       git \
-      pip3
-#    sudo apt-add-repository --yes --update ppa:ansible/ansible
-#    sudo apt install --yes ansible
+      python3-pip
+    sudo pip3 install ansible
   fi
 }
 
